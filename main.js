b2bb4960ff3b4d530745a64b1ded5cd0acce63ce
DENIS SAVGIR <denis@DENISs-MBP.home>
2020-03-30 09:41:38 -0400

    Done

--- main.js
+++ main.js
@@ -2,7 +2,53 @@
  * YOUR CODE HERE *
  ******************/
 
+const isOdd = function(num) {
+  return num % 2 != 0;
+}
 
+const onlyOdds = function(nums) {
+  return nums.filter(isOdd);
+}
+
+const isEven = function(num) {
+  return !isOdd(num);
+}
+
+const onlyEvens = function(nums) {
+  return nums.filter(isEven);
+}
+
+const isShort = function(str) {
+  return str.length < 7;
+}
+
+const shortNamesOnly = function(names) {
+  return names.filter(isShort);
+}
+
+const isDName = function(name) {
+  return name[0].toLowerCase() == 'd';
+}
+
+const dNames = function(names) {
+  return names.filter(isDName);
+}
+
+const containsLowercase = function(str) {
+  return !!str.match(/[a-z]/);
+}
+
+const removeTrolls = function(posts) {
+  return posts.filter(containsLowercase);
+}
+
+const isTriStateArea = function(address2) {
+  return !!address2.match(/(CT|NJ|NY)$/);
+}
+
+function triStateAreaOnly(entries) {
+  return entries.filter(isTriStateArea);
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
